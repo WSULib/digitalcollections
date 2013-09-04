95871b2befe332fd0e1cbee90d59b6df56083de3
root <root@Silo.lib.wayne.edu>
2013-09-04 14:15:42 -0400

    first commit

---
+++ js/singleObject.js
@@ -0,0 +1,118 @@
+//generic functions for singleObject page
+
+var APIdata = new Object();
+APIdata.fineTuning = {};
+
+function APIcall(PID){	
+	
+  // Calls API functions
+	var APIcallURL = "http://silo.lib.wayne.edu/api/index.php?functions='getObjectXML hasMemberOf isMemberOfCollection solr4FedObjsID'&PID="+PID;
+  // removed "getSiblings" in favor of Solr approach
+
+    $.ajax({          
+      url: APIcallURL,      
+      dataType: 'json',            
+      success: callSuccess,
+      error: callError
+    });
+
+    function callSuccess(response){
+    	console.log(response);  
+      APIdata = response;
+
+      //check object status
+      if (APIdata.getObjectXML.object_status == "Inactive" || APIdata.getObjectXML.object_status == "Absent"){
+        loadError();
+      }
+      else{
+        renderTemplates();
+        $("#container").show();              
+      }
+    	
+    }
+
+    function callError(response){
+    	console.log("API Call unsuccessful.  Back to the drawing board.");
+      loadError();                
+    }
+}
+
+function loadError(){
+  $("#container").empty();  
+  var template = '<h3 style="text-align:center;">Could not find Object in WSUDOR.</h3>';
+  var html = Mustache.to_html(template, APIdata);
+  $("#container").html(html);
+  $("#container").show();  
+}
+
+
+// Render Templates with API call data
+function renderTemplates(){  
+  $(document).ready(function(){
+    $.Mustache.addFromDom() //read all template from DOM    
+    // Metadata
+    $('#metadata').mustache('metadata_t', APIdata);
+    // Children
+    if (APIdata.hasMemberOf.results.length > 0){
+      $('#children').mustache('children_t', APIdata);   
+    }
+    else {
+      $('#children').append("<p>This object has no sub-components.</p>");   
+    }
+    // Parents
+    if (APIdata.isMemberOfCollection.results.length > 0){      
+      $('#parents').mustache('parents_t', APIdata);   
+    }
+    else {
+      $('#parents').append("<p>This object is not part of any collections.</p>");   
+    }
+    // Siblings
+    $('#siblings').mustache('siblings_t', APIdata);        
+  });
+
+  finishRendering();
+
+}
+
+
+// Updates and Secondary API calls are performed here
+function finishRendering(){
+  // Content Type Handling
+  ctype = APIdata.solr4FedObjsID.response.docs[0].rels_hasContentModel[0];
+  switch (ctype) {
+    //Images
+    case "info:fedora/Image":
+      var template = '<a href="http://silo.lib.wayne.edu/fedora/objects/{{APIParams.PID}}/datastreams/ACCESS/content"><img src="http://silo.lib.wayne.edu/fedora/objects/{{APIParams.PID}}/datastreams/ACCESS/content"/></a>';
+      var html = Mustache.to_html(template, APIdata);
+      $("#preview_container").html(html);
+      break;
+    //Collections
+    case "info:fedora/Collection":
+      var template = '<a href="http://silo.lib.wayne.edu/digitalcollections/collectionPage.php?PID={{APIParams.PID}}"><img src="http://silo.lib.wayne.edu/fedora/objects/{{APIParams.PID}}/datastreams/THUMBNAIL/content"/></a>';
+      var html = Mustache.to_html(template, APIdata);
+      $("#preview_container").html(html);      
+      break;
+    //eBooks
+    case "info:fedora/wayne:CMWSUebook":
+      var template = '<iframe src="http://silo.lib.wayne.edu/eTextReader/eTextReader.php?ItemID={{APIParams.PID}}#page/1/mode/2up" width="600px" height="500px" frameborder="0" ></iframe>'
+      var html = Mustache.to_html(template, APIdata);
+      $("#preview_container").html(html);
+      break;
+  }
+
+
+}
+
+
+
+function solrSiblings(){
+  var APIcallURL = "http://silo.lib.wayne.edu/api/index.php?functions='Cole's Awesome New Function'&PID="+PID;
+  $.ajax({          
+      url: APIcallURL,      
+      dataType: 'json',            
+      success: callSuccess,
+      error: callError
+    });
+  //append results to APIdata as "solrSibs"
+}
+
---
+++ singleObject.php
@@ -0,0 +1,133 @@
+<?php 
+//Barebones of Single Object Page
+$objectPID = $_REQUEST['PID'];
+?>
+
+<html>
+<head>
+	<!--jquery-->
+	<script src="http://code.jquery.com/jquery.js"></script>
+
+	<!--load bootstrap js-->    
+    <script src="inc/bootstrap/js/bootstrap.js"></script>        
+
+    <!-- Bootstrap core CSS -->
+    <link href="inc/bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="inc/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!--Mustache-->
+    <script src="inc/jquery-Mustache/jquery.mustache.js"></script>
+	<script type="text/javascript" src="inc/mustache.js"></script>
+
+    <!-- Local JS -->
+    <script src="js/singleObject.js"></script>
+
+    <!-- Local CSS -->
+	<link href="css/style.css" rel="stylesheet">
+
+	<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="../assets/js/html5shiv.js"></script>
+    <![endif]-->
+    
+
+
+</head>
+
+<body>
+
+	<div id="container" class="container-fluid">      
+    	
+		<!--row1-->
+    	<div class="row-fluid">
+
+    		<!--image preview testing-->
+    		<div id="preview" class="templated span6">
+    			<h3>Preview</h3>    			
+                <div id="preview_container"></div>
+			</div>
+
+    		<!--metadata testing-->
+    		<div id="metadata" class="templated span6">
+    			<h3>Metadata</h3>
+    			<script id="metadata_t" type="text/html">
+					<p><strong>Title: </strong>{{solr4FedObjsID.response.docs.0.dc_title.0}}</p>					
+					<p><strong>Description: </strong>{{solr4FedObjsID.response.docs.0.dc_description.0}}</p>
+                    <p><strong>Date: </strong>{{solr4FedObjsID.response.docs.0.dc_date.0}}</p>
+                    <p><strong>Language: </strong>{{solr4FedObjsID.response.docs.0.dc_language.0}}</p>
+                    <p><strong>Rights: </strong>{{solr4FedObjsID.response.docs.0.dc_rights.0}}</p>                    
+                    {{#solr4FedObjsID.response.docs.0.dc_subject}}
+                    <p><strong>Subject: </strong>{{.}}</p>
+                    {{/solr4FedObjsID.response.docs.0.dc_subject}}
+                    <p><span style="color:green; font-weight:bold;">Content Type: {{solr4FedObjsID.response.docs.0.rels_hasContentModel.0}}</span></p>
+				</script>   			
+			</div>
+
+		</div>
+
+		<!--row2-->
+    	<div class="row-fluid">
+
+    		<!--children testing-->
+    		<div id="children" class="templated span6">
+    			<h3>Components:</h3>
+                <script id="children_t" type="text/html">    				
+                    {{#hasMemberOf.results}}    					
+						<p><a href="http://silo.lib.wayne.edu/digitalcollections/singleObject.php?PID={{object}}">{{memberTitle}}</a></p>
+					{{/hasMemberOf.results}}					
+				</script> 
+			</div>
+
+    		<!--parents testing-->
+    		<div id="parents" class="span6">    			
+                <h3>Part of Collection(s):</h3>
+                <script id="parents_t" type="text/html">                   
+                    {{#isMemberOfCollection.results}}                                                
+                        <p><a href="http://silo.lib.wayne.edu/digitalcollections/singleObject.php?PID={{subject}}">{{collectionTitle}}</a></p>
+                    {{/isMemberOfCollection.results}}                    
+                </script> 
+			</div>
+
+		</div>
+
+		<!--row3-->
+    	<div class="row-fluid">
+
+    		<!--siblings testing-->
+    		<div id="siblings" class="span12">
+                <h3>Siblings</h3>                
+                <script id="siblings_t" type="text/html">
+                    {{#solr4FedObjsID.response.docs.0.rels_isMemberOfCollection}}
+                        <p><strong>Parent Collection: </strong>{{.}}</p>
+                        <ul>
+                            {{#solrSiblings.info:fedora/wayne:collectionCFAI}}
+                                <li><a href="http://silo.lib.wayne.edu/digitalcollections/singleObject.php?PID={{.}}"><img src="http://silo.lib.wayne.edu/fedora/objects/{{.}}/datastreams/THUMBNAIL/content"/></a></li>
+                            {{/solrSiblings.info:fedora/wayne:collectionCFAI}}
+                        </ul>
+                    {{/solr4FedObjsID.response.docs.0.rels_isMemberOfCollection}}                    
+                </script>
+			</div>    		
+
+		</div>
+
+		<!--row4 Related-->
+        <!--
+    	<div class="row-fluid">    		
+    		<div id="related" class="span12">
+    			<h3>Related</h3>
+			</div>
+		</div>
+        -->
+		
+	</div>
+
+	<!--Testing API call-->
+	<script type="text/javascript">
+		$(document).ready(function(){
+			var PID = "<?php echo $objectPID; ?>";			
+			APIcall(PID);	
+		})		
+	</script>
+
+</body>
+</html>
