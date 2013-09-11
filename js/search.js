f530518e0e1d854e9e078cdb71535998aa623408
root <root@Silo.lib.wayne.edu>
2013-09-11 11:26:22 -0400

    updating search portion

--- js/search.js
+++ js/search.js
@@ -1,30 +1,41 @@
 // Javascript for search view
 
-// Globals
-var searchBlob = {};
-searchBlob.rows = 10;
-searchBlob.start = 0;
-searchBlob.wt = "json";
-searchBlob.facet = 'true';
-searchBlob.facets = [];
-searchBlob.facets.push("dc_date","dc_subject");
-searchBlob['facet.mincount'] = 2;
+// Default Search Parameters (pre form submission)
+var searchDefs = {};
+searchDefs.rows = 10;
+searchDefs.start = 0;
+searchDefs.wt = "json";
+searchDefs.facet = 'true';
+searchDefs.facets = [];
+searchDefs.facets.push("dc_date","dc_subject","dc_creator","dc_language");
+searchDefs['facet.mincount'] = 2;
 
-
+// Global API response data
 APIdata = new Object();
 
-function searchGo(q){
-	//update searchBlob
-	var q = $("#q").val();
-	searchBlob.q = q;
-	console.log(searchBlob);	
+
+// QUERYING
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function searchGo(){
+	
+	console.log("searchParams:");
+	console.log(searchParams);
+	console.log("searchDefs:");
+	console.log(searchDefs);
+
+	// pre-merge
+	// push default facets to params, such that they don't overwrite? May not be necessary, facets should be hardcoded...
+
+	// merge defaults and URL parameters
+	var mergedParams = jQuery.extend(true,{},searchDefs,searchParams);	
+	console.log("mergedParams:");
+	console.log(mergedParams);
 	
 	//pass solr parameters os stringify-ed JSON, accepted by Python API as dicitonary
-	solrParamsString = JSON.stringify(searchBlob);
+	solrParamsString = JSON.stringify(mergedParams);
 
 	// Calls API functions
-	var APIcallURL = "http://silo.lib.wayne.edu/api/index.php?functions='solrSearch'&GETparams='"+solrParamsString+"'";		
-	console.log(APIcallURL);
+	var APIcallURL = "http://silo.lib.wayne.edu/api/index.php?functions='solrSearch'&GETparams='"+solrParamsString+"'";			
 
 	$.ajax({          
 	  url: APIcallURL,      
@@ -35,10 +46,11 @@ function searchGo(q){
 
 	function callSuccess(response){		
 	    APIdata = response;
+	    console.log("APIdata");
 	    console.log(APIdata);
 	    $(document).ready(function(){
-	    	populateResults();
-	    	populateFacets();	
+	    	populateFacets();
+	    	populateResults();	    		
 	    });
 	    
 	}
@@ -46,6 +58,60 @@ function searchGo(q){
 	function callError(response){
 		console.log("API Call unsuccessful.  Back to the drawing board.");	  
 	}
+}
+
+
+function updateSearch(){
+
+	//write params to URL?
+	// http://stackoverflow.com/questions/5997450/append-to-url-and-refresh-page
+
+	// get current URL
+	var cURL = document.URL;
+
+	// check rows to update
+	searchParams.rows = $("#rows").val();
+	var nURL = updateURLParameter(window.location.href, 'rows', searchParams.rows);
+
+	// refresh page	
+	console.log(nURL);
+	window.location = nURL;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// DISPLAY RESULTS
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function populateFacets(){
+		
+	var facetHash = {
+		"dc_date":"Date",
+		"dc_subject":"Subject",
+		"dc_creator":"Creator",
+		"dc_language":"Language"
+	}
+
+	//for each facet field
+	for (var facet in APIdata.solrSearch.facet_counts.facet_fields) {		
+
+		$("#facets_container").append("<div id='"+facetHash[facet]+"_facet'><p><strong>"+facetHash[facet]+"</strong></p><ul class='facet_list' id='"+facetHash[facet]+"_list'</div>");
+		
+		var facet_array = APIdata.solrSearch.facet_counts.facet_fields[facet];
+		for (var i = 0; i < facet_array.length; i = i + 2){		
+			$("#"+facetHash[facet]+"_list").append("<li>"+facet_array[i]+" - "+facet_array[i+1]+"</li>");
+		}		
+
+	}
+		
 }
 
 function populateResults(){
@@ -66,20 +132,30 @@ function populateResults(){
 	}	
 }
 
-function populateFacets(){
-	
-	//push results to results_container
-	for (var i = 0; i < APIdata.solrSearch.response.docs.length; i++) {		
 
-  		$.ajax({          
-		  url: 'templates/searchResultObj.htm',      
-		  dataType: 'html',            
-		  async:false,
-		  success: function(response){		  	
-		  	var template = response;
-		  	var html = Mustache.to_html(template, APIdata.solrSearch.response.docs[i]);		  	
-		  	$("#results_container").append(html);
-		  }		  
-		});
-	}	
+
+
+// UTILITIES
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/**
+ * http://stackoverflow.com/a/10997390/11236
+ */
+function updateURLParameter(url, param, paramVal){
+    var newAdditionalURL = "";
+    var tempArray = url.split("?");
+    var baseURL = tempArray[0];
+    var additionalURL = tempArray[1];
+    var temp = "";
+    if (additionalURL) {
+        tempArray = additionalURL.split("&");
+        for (i=0; i<tempArray.length; i++){
+            if(tempArray[i].split('=')[0] != param){
+                newAdditionalURL += temp + tempArray[i];
+                temp = "&";
+            }
+        }
+    }
+
+    var rows_txt = temp + "" + param + "=" + paramVal;
+    return baseURL + "?" + newAdditionalURL + rows_txt;
 }
--- search.php
+++ search.php
@@ -1,38 +1,25 @@
-<?php 
-//Barebones of Single Object Page
-$q = $_REQUEST['q'];
-?>
-
 <html>
 <head>
     <title>WSUDOR - Search</title>
 
 	<!--jquery-->
 	<script src="http://code.jquery.com/jquery.js"></script>
-
 	<!--load bootstrap js-->    
-    <script src="inc/bootstrap/js/bootstrap.js"></script>        
-
+    <script src="inc/bootstrap/js/bootstrap.js"></script>
     <!-- Bootstrap core CSS -->
     <link href="inc/bootstrap/css/bootstrap.css" rel="stylesheet">
     <link href="inc/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
-
     <!--Mustache-->
     <script src="inc/jquery-Mustache/jquery.mustache.js"></script>
 	<script type="text/javascript" src="inc/mustache.js"></script>
-
     <!-- Local JS -->
     <script src="js/search.js"></script>
-
     <!-- Local CSS -->
 	<link href="css/style.css" rel="stylesheet">
-
 	<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
       <script src="../assets/js/html5shiv.js"></script>
     <![endif]-->
-    
-
 
 </head>
 
@@ -42,17 +29,19 @@ $q = $_REQUEST['q'];
         
         <div class="row-fluid" id="search_form">            
             <div class="span4 pull-right">
-                <form class="form-search" onsubmit="searchGo(); return false;">                    
-                    <input style="height:30px;" class="input-medium search-query" id='q' type='text' value="ford" placeholder="e.g. Detroit"/>
+
+                <form class="form-search" action="search.php">                    
+                    <input style="height:30px;" class="input-medium search-query" name='q' id='q' type='text' placeholder="e.g. Detroit"/>
                     <button type="submit" class="btn">Search</button>
                 </form>
+
             </div>
         </div>            
         
         <div class="row-fluid">
             <div class="num_results span12">
                 <span><strong>Number of Results</strong></span></br>
-                <select style="width:75px;" id='rows'>
+                <select style="width:75px;" id='rows' onchange="updateSearch();">
                     <option value="10">10</option>
                     <option value="20">20</option>
                     <option value="50">50</option>
@@ -74,7 +63,7 @@ $q = $_REQUEST['q'];
             <h4>Search Parameters</h4>            
 
             <!--Consider iterating through facets as selected in Limiters object...-->
-            <div id="facets"></div>
+            <div id="facets_container"></div>
         
         </div><!--closes search_parameters-->
 
@@ -93,10 +82,7 @@ $q = $_REQUEST['q'];
 </body>
 
 <script type="text/javascript">
-    var q = "<?php echo $q; ?>";
-    if (q.length > 0){
-        $("#q").val(q);
-        searchGo();
-    }
+    var searchParams = <?php echo json_encode($_REQUEST); ?>;    
+    searchGo();
 </script>
 </html>
