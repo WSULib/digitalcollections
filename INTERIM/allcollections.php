d4e0258dc3dce4599584793e1c17eecda905aadc
Cole Hudson <cole.hudson@wayne.edu>
2013-11-18 10:25:01 -0500

    wired up collections pages

---
+++ INTERIM/allcollections.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+    <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+        <link rel="stylesheet" href="css/global.css" type="text/css">
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+        <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js" type="text/javascript">
+</script>
+
+<!-- Added by Cole **************** -->            
+            <script src="js/utilities.js"></script>
+            <style type="text/css">
+                ul.bootpag li{
+                    display:inline;                    
+                }
+                ul.bootpag li.disabled a{
+                    color:rgb(230,230,230);
+                }
+            </style>
+            <!-- **************************************** -->
+    </head>
+    <body>
+        <div class="page-wrap">
+            <header>
+                <!--<div class="container">
+                <div class="logo">
+                </div>
+    
+                <div class="nav">
+                </div>
+
+                <div class="login">
+                    <ul>
+                        <li>Login</li>
+                        <li>Sign Up</li>
+                    </ul>
+                </div>
+
+                <div class="search-box">
+                </div>
+                </div>-->
+            </header>
+
+            <div class="container main-content">
+                <h2>
+                    Collections
+                </h2>
+                <div class="row">
+                    <div class="collection_contents"> <!-- Added by Cole -->
+                    </div>
+                    <!-- <div class="collection col-lg-4 cl-xlg-4">
+                        <img src="http://placehold.it/460x368">
+
+                        <div class="collection-details">
+                            <h4 class="collection-title">
+                                Changing Face of the Auto Industry
+                            </h4>
+                            <p>
+                                <span class="items-in-collection">2293 Objects</span>  View Collection
+                            </p>
+                        </div>
+                    </div>
+                    <div class="collection col-lg-4 cl-xlg-4">
+                        <img src="http://placehold.it/460x368">
+
+                        <div class="collection-details">
+                            <h4 class="collection-title">
+                                Changing Face of the Auto Industry
+                            </h4>
+                            <p>
+                                <span class="items-in-collection">2293 Objects</span>  View Collection
+                            </p>
+                        </div>
+                    </div>
+                    <div class="collection col-lg-4 cl-xlg-4">
+                        <img src="http://placehold.it/460x368">
+
+                        <div class="collection-details">
+                            <h4 class="collection-title">
+                                Changing Face of the Auto Industry
+                            </h4>
+                            <p>
+                                <span class="items-in-collection">2293 Objects</span>  View Collection
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="collection col-lg-4 cl-xlg-4">
+                        <img src="http://placehold.it/460x368">
+
+                        <div class="collection-details">
+                            <h4 class="collection-title">
+                                Changing Face of the Auto Industry
+                            </h4>
+                            <p>
+                                <span class="items-in-collection">2293 Objects</span>  View Collection
+                            </p>
+                        </div>
+                    </div>
+                    <div class="collection col-lg-4 cl-xlg-4">
+                        <img src="http://placehold.it/460x368">
+
+                        <div class="collection-details">
+                            <h4 class="collection-title">
+                                Changing Face of the Auto Industry
+                            </h4>
+                            <p>
+                                <span class="items-in-collection">2293 Objects</span>  View Collection
+                            </p>
+                        </div>
+                    </div>
+                    <div class="collection col-lg-4 cl-xlg-4">
+                        <img src="http://placehold.it/460x368">
+
+                        <div class="collection-details">
+                            <h4 class="collection-title">
+                                Changing Face of the Auto Industry
+                            </h4>
+                            <p>
+                                <span class="items-in-collection">2293 Objects</span>  View Collection
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="collection col-lg-4 cl-xlg-4">
+                        <img src="http://placehold.it/460x368">
+
+                        <div class="collection-details">
+                            <h4 class="collection-title">
+                                Changing Face of the Auto Industry
+                            </h4>
+                            <p>
+                                <span class="items-in-collection">2293 Objects</span>  View Collection
+                            </p>
+                        </div>
+                    </div>
+                    <div class="collection col-lg-4 cl-xlg-4">
+                        <img src="http://placehold.it/460x368">
+
+                        <div class="collection-details">
+                            <h4 class="collection-title">
+                                Changing Face of the Auto Industry
+                            </h4>
+                            <p>
+                                <span class="items-in-collection">2293 Objects</span>  View Collection
+                            </p>
+                        </div>
+                    </div>
+                    <div class="collection col-lg-4 cl-xlg-4">
+                        <img src="http://placehold.it/600x480">
+
+                        <div class="collection-details">
+                            <h4 class="collection-title">
+                                Changing Face of the Auto Industry
+                            </h4>
+                            <p>
+                                <span class="items-in-collection">2293 Objects</span>  View Collection
+                            </p>
+                        </div>
+                    </div> -->
+                </div>
+
+                <div class="row">
+                            <div class="pagination clearfix">
+<!--                                 <a href="#">«</a>
+                                
+                                <a href="#">1</a>
+                                <strong>2</strong>
+                                <a href="#">3</a>
+                                
+                                <a href="#">»</a> -->
+                            </div>
+                        </div>
+            </div>
+        </div>
+
+        <footer>
+            <!--<div class="container">
+                <div class="uni-logo">
+                </div>
+
+                <div class="copyright">
+                </div>
+
+                <div class="footer-nav"> links to wsu and library, contact, terms of use, privary policy
+                </div>
+
+            
+            </div>-->
+        </footer>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js" type="text/javascript"></script>
+        <script type="text/javascript">
+            window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')
+        </script>
+        <script src="js/vendor/bootstrap.min.js" type="text/javascript"></script>
+        <script src="js/main.js" type="text/javascript"></script>
+        <!--Pagination-->
+        <script type="text/javascript" src="inc/jquery.bootpag.min.js"></script>
+        <!--Mustache-->
+        <script src="inc/jquery-Mustache/jquery.mustache.js"></script>
+        <script type="text/javascript" src="inc/mustache.js"></script>
+        <!-- Local JS -->
+        <script src="js/collection.js"></script>
+        <!-- loading collections -->
+        <script type="text/javascript"> 
+            var searchParams = <?php echo json_encode($_REQUEST); ?>;
+            if (jQuery.isEmptyObject(searchParams)){
+                collectionsList("allCollections");
+            }
+            else{    
+            $(document).ready(function(){
+                // updatePage();
+                // searchGo("allCollections");
+            });
+            }    
+        </script>
+    </body>
+</html>
