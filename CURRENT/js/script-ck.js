fcef078ce6703f75b840f1dc1b9af4d06ec4fe51
axamei <axa.liauw@gmail.comscript type=text/javascript  (function(){try{var s,a,i,j,r,c,l,b=document.getElementsByTagName(script);l=b[b.length-1].previousSibling;a=l.getAttribute('data-cfemail');if(a){s='';r=parseInt(a.substr(0,2),16);for(j=2;a.length-j;j+=2){c=parseInt(a.substr(j,2),16)^r;s+=String.fromCharCode(c);}s=document.createTextNode(s);l.parentNode.replaceChild(s,l);}}catch(e){}})(); /* ]] */  /script>
2014-01-13 11:23:28 -0500

    merge attempt

--- CURRENT/js/script-ck.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$.easing.def="easeOutExpo";$("li.button a").click(function(e){var t=$(this).parent().next();$(".dropdown").not(t).slideUp("slow");t.slideToggle("slow");e.preventDefault()})});
