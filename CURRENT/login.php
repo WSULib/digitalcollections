144f9e30d79b3dfb826012b93bf654b85c3edcec
axamei <axa.liauw@gmail.comscript type=text/javascript  (function(){try{var s,a,i,j,r,c,l,b=document.getElementsByTagName(script);l=b[b.length-1].previousSibling;a=l.getAttribute('data-cfemail');if(a){s='';r=parseInt(a.substr(0,2),16);for(j=2;a.length-j;j+=2){c=parseInt(a.substr(j,2),16)^r;s+=String.fromCharCode(c);}s=document.createTextNode(s);l.parentNode.replaceChild(s,l);}}catch(e){}})(); /* ]] */  /script>
2014-01-15 16:25:24 -0500

    login

--- CURRENT/login.php
+++ CURRENT/login.php
@@ -3,134 +3,153 @@
 $objectPID = $_REQUEST['PID'];
 ?>
 
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!-->
-<html class="no-js">
-    <!--<![endif]-->
+<!DOCTYPE php>
+<html>
     <head>
-
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>WSUDOR Login</title>
+        <title>Digital Collections - Wayne State University Libraries</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width">
-        <link rel="stylesheet" href="css/global.css" type="text/css">
-        <link rel="stylesheet" href="ico/style.css" type="text/css">
-        <link rel="stylesheet" href="css/style.css" type="text/css">
-        <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+
+        <link rel="stylesheet" href="css/main.css" type="text/css">
+        
+        <!-- Typography -->
+        <link href='http://fonts.googleapis.com/css?family=Roboto:400,900,100,100italic,300,300italic,400italic,500,500italic,700,700italic' rel='stylesheet' type='text/css'>
+
         <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js" type="text/javascript"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js" type="text/javascript"></script>
+
+        <!--Mustache-->
+        <script src="inc/jquery-Mustache/jquery.mustache.js"></script>
+        <script type="text/javascript" src="inc/mustache.js"></script>
+
+        <script src="inc/jquery.cookie.js" type="text/javascript"></script>           
+        
+        <!-- Local JS -->
+        <script src="js/main.js" type="text/javascript"></script>
+        <script src="js/utilities.js"></script>        
+        <script src="js/login.js"></script>
+        <script src="js/userData.js"></script>
+        
+        <!--WSUDOR Translation Dictionary-->
+        <script type="text/javascript" src="js/rosettaHash.js"></script>
+        
+        <!--Pagination-->
+        <script type="text/javascript" src="inc/jquery.bootpag.min.js"></script> 
+        
+        <script src="js/vendor/bootstrap.min.js" type="text/javascript"></script>
+
+<script>
+(function( $ ) {
+  // constants
+  var SHOW_CLASS = 'show',
+      HIDE_CLASS = 'hide',
+      ACTIVE_CLASS = 'active';
+  
+  $( '.tabs' ).on( 'click', 'li a', function(e){
+    e.preventDefault();
+    var $tab = $( this ),
+         href = $tab.attr( 'href' );
+  
+     $( '.active' ).removeClass( ACTIVE_CLASS );
+     $tab.addClass( ACTIVE_CLASS );
+  
+     $( '.show' )
+        .removeClass( SHOW_CLASS )
+        .addClass( HIDE_CLASS )
+        .hide();
+    
+      $(href)
+        .removeClass( HIDE_CLASS )
+        .addClass( SHOW_CLASS )
+        .hide()
+        .fadeIn( 550 );
+  });
+})( jQuery );
+</script>
 
     </head>
-    <!-- Additions ###################################################################################### -->
-            <!--jquery-->            
-            <script src="http://code.jquery.com/jquery.js"></script>
-            <!--Mustache-->
-            <script src="inc/jquery-Mustache/jquery.mustache.js"></script>
-            <script type="text/javascript" src="inc/mustache.js"></script>  
-            <!--cookie.js-->
-            <script src="inc/jquery.cookie.js"></script>                      
-            <!-- Local JS -->
-            <script src="js/utilities.js"></script>        
-            <script src="js/login.js"></script>
-            <script src="js/userData.js"></script>
-            <!--WSUDOR Translation Dictionary-->
-            <script type="text/javascript" src="js/rosettaHash.js"></script>
-            <!--Pagination-->
-            <script type="text/javascript" src="inc/jquery.bootpag.min.js"></script>            
-            
-
-            <!-- Temporary CSS -->
-            <style type="text/css">
-                #login_form_container{
-                    float:center;
-                    margin:auto;
-                    background-color:rgba(242,242,242,1);
-                    margin-top:20px;
-                    padding:10px;
-                }
-                #messages_container{
-                    float:center;
-                    margin:auto;
-                    background-color:rgba(242,242,242,1);
-                    margin-top:20px;
-                    padding:10px;
-                }
-                #createAccount_container{
-                    float:center;
-                    margin:auto;
-                    background-color:rgba(242,242,242,1);
-                    margin-top:20px;
-                    padding:10px;
-                    display:none;
-                }
-                a{
-                    color:blue;
-                }
-            </style>
-
-        <!-- Additions ###################################################################################### -->
     <body>
-        <div class="page-wrap">
-            <header>
-                
-            </header>
 
-            <div id="templateCM">
-
-                <div class="container">                    
-
-                    <div id="login_form_container" class="primary-object-container col-lg-9 col-xlg-9">                        
-                        <h2>Hello Login World.</h2>
-                        <hr>                    
-                        <div id="login_form" class="col-lg-12 col-xlg-12">
-                            <p>Wayne State user? Login with your accessID and password.</p>
-                            <p>Community member?  General public?  Login with your username and password.</p>
-                            <form onsubmit="loginForm(); return false;">                                
-                                accessID or username: <input id="username" type="text" name="username"><br>
-                                password: <input id="password" type="password" name="password">
-                                <input type='submit'/></input>
-                            </form>
-                            <hr>
-                        </div>                        
-                        <div id="login_instructions" class="col-lg-12 col-xlg-12">
-                            <p>Don't have an account? Click <a href="#" onclick="$('#createAccount_container').fadeToggle(); return false;">here</a> to create one.</p>
-                        </div>
-                    </div>
-
-                    <div id="createAccount_container" class="primary-object-container col-lg-9 col-xlg-9">                        
-                        <h2>Create a new account</h2>
-                        <p style="color:green;">Remember, you can login with your WSU credentials above!</p>
-                        <p>* required</p>
-                        <form onsubmit="createAccountPrep('userDefined'); return false;">                                
-                            * display name: <input id="create_displayName" type="text" name="create_displayName"><br>
-                            * username: <input id="create_username" type="text" name="create_username"><br>
-                            * password: <input id="create_password" type="password" name="create_password">
-                            <input type='submit'/></input>
-                        </form>
-                        <hr>                            
-                    </div>   
-
-                    <div id="messages_container" class="primary-object-container col-lg-9 col-xlg-9">                        
-                        <h2>Messages...</h2>
-                        <hr>                            
-                    </div>                  
-
-                </div>                
-
-            </div> <!-- closes templateCM -->
-
-        </div> <!-- page wrap -->
-
-        <footer>            
-        </footer>        
+    <div class="container" id="templateCM">
+        <h1 class="brand-login">
+            <a href="collection.php">Digital Collections</a>
+        </h1>
+        <div class="flat-form">
+            <ul class="tabs">
+                <li>
+                    <a href="#login_form_container" class="active">Login</a>
+                </li>
+                <li>
+                    <a href="#createAccount_container">Sign Up</a>
+                </li>
+            </ul>
+            <div id="login_form_container" class="form-action show">
+                <h1>Hello World.</h1>
+                <div id="login_form">
+                    <p>Login with your AccessID/Username and Password. If you don't have an AccessID or Username, please sign up!</p>
+                    <form onsubmit="loginForm(); return false">
+                        <ul>
+                            <li><input id="username" type="text" name="username" placeholder="Username"></li>
+                            <li><input id="password" type="password" name="password" placeholder="Password"></li>
+                            <li><input type="submit" value="Login" class="button"></li>
+                        </ul>
+                    </form>
+                </div>
+            </div>
+            <!--/#login.form-action-->
+            <div id="createAccount_container" class="form-action hide">
+                <h1>Register.</h1>
+                <p>
+                    If you don't have a WSU accessID, you should totes sign up for an account. It's what all the cool kids are doing.
+                </p>
+                <form onsubmit="createAccountPrep('userDefined'); return false;">
+                    <ul>
+                        <li>
+                            <input id="create_displayName" type="text" name="create_displayName" placeholder="Username" />
+                        </li>
+                        <li>
+                            <input id="create_username" type="password" name="create_username" placeholder="Password" />
+                        </li>
+                        <li>
+                            <input type="submit" value="Sign Up" class="button" />
+                        </li>
+                    </ul>
+                </form>
+            </div>
+            <!--/#register.form-action-->
+        </div>
+    </div>  
         
     </body>
-           
-    <script src="js/vendor/bootstrap.min.js" type="text/javascript"></script>
-    <script src="js/main.js" type="text/javascript"></script> 
+
+    <script class="cssdeck">(function( $ ) {
+  // constants
+  var SHOW_CLASS = 'show',
+      HIDE_CLASS = 'hide',
+      ACTIVE_CLASS = 'active';
+  
+  $( '.tabs' ).on( 'click', 'li a', function(e){
+    e.preventDefault();
+    var $tab = $( this ),
+         href = $tab.attr( 'href' );
+  
+     $( '.active' ).removeClass( ACTIVE_CLASS );
+     $tab.addClass( ACTIVE_CLASS );
+  
+     $( '.show' )
+        .removeClass( SHOW_CLASS )
+        .addClass( HIDE_CLASS )
+        .hide();
+    
+      $(href)
+        .removeClass( HIDE_CLASS )
+        .addClass( SHOW_CLASS )
+        .hide()
+        .fadeIn( 550 );
+  });
+})( jQuery );</script>
+
 
     <!--API call-->
     <script type="text/javascript">
@@ -139,6 +158,4 @@ $objectPID = $_REQUEST['PID'];
         })      
     </script>
     
-
-
 </html>
