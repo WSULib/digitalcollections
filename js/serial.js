e7e590043940f6b55d790d9b6273236dfc0edd50
Graham Hukill <ghukill@gmail.com>
2014-02-03 16:53:34 -0500

    headway on serial nav

--- js/serial.js
+++ js/serial.js
@@ -8,7 +8,7 @@ APIdata = new Object();
 function launch(PID){
 	
 	// returns serial object metadata	
-	var URL = "http://silo.lib.wayne.edu/WSUAPI?functions[]=getObjectXML&functions[]=hasMemberOf&functions[]=isMemberOfCollection&functions[]=solrGetFedDoc&PID="+PID;
+	var URL = "http://silo.lib.wayne.edu/WSUAPI-dev?functions[]=getObjectXML&functions[]=serialWalk&functions[]=isMemberOfCollection&functions[]=solrGetFedDoc&PID="+PID;
 
 	$.ajax({          
 		url: URL,      
@@ -18,18 +18,29 @@ function launch(PID){
 	});
 
 	function callSuccess(response){
-		console.log("Serial metadata response:",response);  
+		console.log("Serial metadata response:",response);		
 		APIdata.serialMeta = response;
+		APIdata.serialMeta.cleanVols = cleanSerialWalk(response.serialWalk.results);		
+		APIdata.counter = new Object();
+		APIdata.counter = {
+			'val':1,
+			'count' : function () {
+            return function (text, render) {
+	                // note that counter is in the enclosing scope
+	                return APIdata.counter.val++;	                	                
+            	}
+        	}
+    	}
 
 		//check object status
 		if (APIdata.serialMeta.getObjectXML.object_status == "Inactive" || APIdata.serialMeta.getObjectXML.object_status == "Absent"){
-		  loadError();
+			loadError();
 		}
 		else{
 	  		// render serialBlockNav
 			renderSerialNavBlock();
 			// render serial-root-content
-			renderMain();                    
+			renderMain();
 		}
 			
 	}
@@ -40,15 +51,43 @@ function launch(PID){
 	
 }
 
+function cleanSerialWalk(results){
 
+	var vols = {};
+	var volsArray = [];	
+	var sortingArray = [];
+	for (var i=0; i<results.length; i++){
+		var node = results[i];
+		if (sortingArray.indexOf(node.volume) == -1){
+			sortingArray.push(node.volume);
+		}			
+		if (vols.hasOwnProperty(node.volume) == false ){
+			vols[node.volume] = [];
+			var temp = [];
+			temp.push(node.issue,node.issueTitle);
+			vols[node.volume].push(temp);			
+		}
+		else{
+			var temp = [];
+			temp.push(node.issue,node.issueTitle);
+			vols[node.volume].push(temp);
+		}
+	}	
+	
+	sortingArray.alphanumSort();
+	APIdata.sortingArray = sortingArray;
+	console.log(APIdata.sortingArray);
+	
+	// pluck to array
+	for (var i=0; i<APIdata.sortingArray.length; i++){
+		volsArray.push(vols[APIdata.sortingArray[i]])
+	}
+
+	return volsArray;
+}
 
 
 function renderSerialNavBlock(){
-
-	// get 2nd level objects, volumes for now
-	// use nested templates?
-
-	var templateData = new Object();
 
 	$.ajax({                
 		url: "templates/serial-nav.htm",      
@@ -56,17 +95,14 @@ function renderSerialNavBlock(){
 		async:true,
 		success: function(response){        
 			var template = response;
-			var html = Mustache.to_html(template, templateData);       
+			var html = Mustache.to_html(template, APIdata);       
 			$("#serial-nav").append(html);
 		}     
 	});
 
 }
 
-function renderMain(){
-
-
-	var templateData = new Object();
+function renderMain(){	
 
 	$.ajax({                
 		url: "templates/serial-root-content.htm",      
