934b29474d8bbfc7d25b51b711d9a93a3d4b1e91
Graham Hukill <ghukill@gmail.com>
2014-02-14 09:51:17 -0500

    routing 'item' working

--- item.php
+++ item.php
@@ -7,33 +7,50 @@
 $PID = $_REQUEST['id'];
 $PID = str_replace(":","\:",$PID);
 
-// get solr rels_isRenderedBy relationship
-$options = array
-(
-    'hostname' => 'localhost',    
-    'port'     => 8080,
-    'path'     => 'solr4/fedobjs'
-);
+// check for PID
+if( isset($_GET["id"]) ){
+   	// get solr rels_isRenderedBy relationship
+	$options = array
+	(
+	    'hostname' => 'localhost',    
+	    'port'     => 8080,
+	    'path'     => 'solr4/fedobjs'
+	);
 
-$client = new SolrClient($options);
-$query = new SolrQuery();
+	$client = new SolrClient($options);
+	$query = new SolrQuery();
 
-$query->setQuery("id:$PID");
+	$query->setQuery("id:$PID");
 
-$query->setStart(0);
-$query->setRows(50);
-$query->addField('id')->addField('rels_isRenderedBy');
-$query_response = $client->query($query);
-$response = $query_response->getResponse();
+	$query->setStart(0);
+	$query->setRows(50);
+	$query->addField('id')->addField('rels_isRenderedBy');
+	$query_response = $client->query($query);
+	$response = $query_response->getResponse();	
 
-$isRenderedBy_string = $response['response']['docs'][0]['rels_isRenderedBy'][0];
-$isRenderedBy_temp = explode("info:fedora/",$isRenderedBy_string);
-$isRenderedBy = $isRenderedBy_temp[1];
-$fileTemplate = $isRenderedBy.".php";
+	if ( isset($response['response']['docs'][0]['rels_isRenderedBy']) ){
+		echo "We got something.  Going there.";
+		$isRenderedBy_string = $response['response']['docs'][0]['rels_isRenderedBy'][0];
+		$isRenderedBy_temp = explode("info:fedora/",$isRenderedBy_string);
+		$isRenderedBy = $isRenderedBy_temp[1];
+		$fileTemplate = $isRenderedBy.".php";				
+	}
 
-// render template
-include $fileTemplate;
+	else {		
+		$fileTemplate = "singleObject.php";
+	}
 
+	
+}
+
+else{
+	echo "PID not provided, this will render 404 page."; 
+	$fileTemplate = "404.php";  	
+}
+
+
+// render selected template
+include $fileTemplate;	
 
 
 ?>
