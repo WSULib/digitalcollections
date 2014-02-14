26d95c3e34113dbe9d1fd34526648c19b3774d39
axamei <axa.liauw@gmail.comscript type=text/javascript  (function(){try{var s,a,i,j,r,c,l,b=document.getElementsByTagName(script);l=b[b.length-1].previousSibling;a=l.getAttribute('data-cfemail');if(a){s='';r=parseInt(a.substr(0,2),16);for(j=2;a.length-j;j+=2){c=parseInt(a.substr(j,2),16)^r;s+=String.fromCharCode(c);}s=document.createTextNode(s);l.parentNode.replaceChild(s,l);}}catch(e){}})(); /* ]] */  /script>
2014-02-14 13:02:18 -0500

    add 404

---
+++ 404.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Page Not Found</title>
+
+	<link rel="stylesheet" href="css/main.css" type="text/css">
+        <link rel="stylesheet" href="inc/sidr/stylesheets/jquery.sidr.dark.css">
+
+        
+        <!-- Typography -->
+        <link href='http://fonts.googleapis.com/css?family=Roboto:400,900,100,100italic,300,300italic,400italic,500,500italic,700,700italic' rel='stylesheet' type='text/css'>
+
+        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js" type="text/javascript"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js" type="text/javascript"></script>
+
+        <!--Mustache-->
+        <script src="inc/jquery-Mustache/jquery.mustache.js"></script>
+        <script type="text/javascript" src="inc/mustache.js"></script>
+        
+        <!-- Local JS -->
+        <script src="js/main.js" type="text/javascript"></script>
+        <script src="js/favorites.js"></script>
+        <script src="js/userData.js"></script>             
+        
+        <script src="js/utilities.js"></script>
+        <script src="js/collection.js"></script>
+        
+        <!--WSUDOR Translation Dictionary-->
+        <script type="text/javascript" src="js/rosettaHash.js"></script>
+        
+        <!--Pagination-->
+        <script type="text/javascript" src="inc/jquery.bootpag.min.js"></script> 
+        
+        <script src="js/vendor/bootstrap.min.js" type="text/javascript"></script>
+        <script src="js/jquery.cookie.js" type="text/javascript"></script> 
+        <script src="inc/sidr/jquery.sidr.min.js"></script>
+</head>
+<body>
+
+	<?php include('inc/header.php'); ?>
+
+	<div class="container">
+		<h2>Page Not Found</h2>
+		<p>The page you were looking for does not exist. Please contact us with any questions.</p>
+	</div>
+	
+	<?php include('inc/footer.php'); ?>
+	
+</body>
+</html>
