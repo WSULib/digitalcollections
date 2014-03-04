ccb83999f867f22b58c8a8c692f753ea6ea4e2d2
axamei <axa.liauw@gmail.comscript type=text/javascript  (function(){try{var s,a,i,j,r,c,l,b=document.getElementsByTagName(script);l=b[b.length-1].previousSibling;a=l.getAttribute('data-cfemail');if(a){s='';r=parseInt(a.substr(0,2),16);for(j=2;a.length-j;j+=2){c=parseInt(a.substr(j,2),16)^r;s+=String.fromCharCode(c);}s=document.createTextNode(s);l.parentNode.replaceChild(s,l);}}catch(e){}})(); /* ]] */  /script>
2014-03-04 14:04:27 -0500

    added contact form

--- inc/contactengine.php
+++ inc/contactengine.php
@@ -9,11 +9,11 @@ $Email = Trim(stripslashes($_POST['email']));
 $Message = Trim(stripslashes($_POST['message'])); 
 
 // validation
-$validationOK=true;
-if (!$validationOK) {
-  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
-  exit;
-}
+// $validationOK=true;
+// if (!$validationOK) {
+//   print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+//   exit;
+// }
 
 // prepare email body text
 $Body = "";
