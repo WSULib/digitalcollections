cdcfcf080e8c72a0aeaf115a5dd03014a8072f96
axamei <axa.liauw@gmail.comscript type=text/javascript  (function(){try{var s,a,i,j,r,c,l,b=document.getElementsByTagName(script);l=b[b.length-1].previousSibling;a=l.getAttribute('data-cfemail');if(a){s='';r=parseInt(a.substr(0,2),16);for(j=2;a.length-j;j+=2){c=parseInt(a.substr(j,2),16)^r;s+=String.fromCharCode(c);}s=document.createTextNode(s);l.parentNode.replaceChild(s,l);}}catch(e){}})(); /* ]] */  /script>
2014-04-22 10:17:45 -0400

    fix footer, favorites, menu icon in small devices

--- js/userData.js
+++ js/userData.js
@@ -45,7 +45,7 @@ $(document).ready(function(){
 			$("li.login_status").html("<a href='favorites.php'><span class='glyphicons user'></span> Welcome, "+userData.displayName+"!</a> <a onclick='logoutUser(); return false;' href='#' class='logout'><span class='glyphicons log_out'></span> Log Out</a>");
 			$("li.sidr-class-login_status").html("<a onclick='logoutUser(); return false;' href='#'><span class='glyphicons log_out'></span> Log Out</a>");
 			$('nav.header-primary ul li:eq(2)').before("<li><a href='favorites.php' id='fav_link'>Favorites</a></li>");
-			$('#sidr-main nav ul li:eq(2)').before("<li><a href='favorites.php' id='fav_link'>Favorites</a></li>");
+			$('#sidr-main div ul li:eq(2)').before("<li><a href='favorites.php' id='fav_link'>Favorites</a></li>");
 		}  
 	}      
 	else {
