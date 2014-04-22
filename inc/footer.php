afb2edc6b865b76a4685db5abd5228d69305a2c0
axamei <axa.liauw@gmail.comscript type=text/javascript  (function(){try{var s,a,i,j,r,c,l,b=document.getElementsByTagName(script);l=b[b.length-1].previousSibling;a=l.getAttribute('data-cfemail');if(a){s='';r=parseInt(a.substr(0,2),16);for(j=2;a.length-j;j+=2){c=parseInt(a.substr(j,2),16)^r;s+=String.fromCharCode(c);}s=document.createTextNode(s);l.parentNode.replaceChild(s,l);}}catch(e){}})(); /* ]] */  /script>
2014-04-22 14:09:19 -0400

    updated links in footer

--- inc/footer.php
+++ inc/footer.php
@@ -3,10 +3,10 @@
         <div class="row">
           <div class="col-xs-6 pull-right">
             <ul class="links">
-              <li><a href="">Libraries</a></li>
-              <li><a href="https://www.surveymonkey.com/s/QHWX3V7">Give Us Feedback</a></li>
-              <li><a href="">Privacy</a></li>
-              <li><a href="">Acceptable Use</a></li>
+              <li><a href="http://www.lib.wayne.edu">Libraries</a></li>
+              <li><a href="https://www.surveymonkey.com/s/QHWX3V7">Feedback</a></li>
+              <li><a href="http://www.lib.wayne.edu/info/policies/privacy.php">Privacy</a></li>
+              <li><a href="http://www.lib.wayne.edu/info/policies/eresources.php">Acceptable Use</a></li>
             </ul>
           </div>
           <div class="col-xs-6 copyright">
