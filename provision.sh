6342a900a3176c6c114e76fe86d753d490ab779c
Cole Hudson <cole.hudson@wayne.edu>
2016-12-19 10:29:46 -0500

    fixes composer install failure due to new version hash

--- provision.sh
+++ provision.sh
@@ -1,12 +1,23 @@
+#!/bin/sh
 # run as sudo
 
-# install composer
-# https://getcomposer.org/download/
-echo "installing composer..."
-sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-sudo php -r "if (hash_file('SHA384', 'composer-setup.php') === 'aa96f26c2b67226a324c27919f1eb05f21c248b987e6195cad9690d5c1ff713d53020a02ac8c217dbf90a7eacc9d141d') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
-sudo php -r "unlink('composer-setup.php');"
+EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+
+if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+then
+    >&2 echo 'ERROR: Invalid installer signature'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+RESULT=$?
+
+rm composer-setup.php
 
 # install dependencies via composer
 composer update
+
+exit $RESULT
