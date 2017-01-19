e26ade82105c2311b28971fca952a2e2b410741d
Cole Hudson <cole.hudson@wayne.edu>
2017-01-19 13:36:08 +0000

    new template file

---
+++ src/settings.template.php
@@ -0,0 +1,35 @@
+<?php
+return [
+    'settings' => [
+        'displayErrorDetails' => true, // set to false in production
+        'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+        'determineRouteBeforeAppMiddleware' => true, // Only set this if you need access to route within middleware
+
+        // Renderer settings
+        'renderer' => [
+            'template_path' => __DIR__ . '/../templates/',
+        ],
+
+        // Monolog settings
+        'logger' => [
+            'name' => 'slim-app',
+            'path' => __DIR__ . '/../logs/app.log',
+            'level' => \Monolog\Logger::DEBUG,
+        ],
+
+        // API settings
+        'API' => [
+            'url' => 'http://localhost/api/',
+            'username' => null,
+            'password' => null,
+        ],
+
+        // contact form settings
+        'contact_form' => [
+            'general' => 'email',
+            'permissions' => 'email',
+            'rap' => 'email',
+            'passphrase' => 'string'
+        ]
+    ],
+];
